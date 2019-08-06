c440ba6458ce77ee64579cbaf7ef8ca64aa7e806
jskisor <kisorgiri@gmail.com>
2019-08-06 09:04:49 +0545

    git file added

---
+++ git.js
@@ -0,0 +1,23 @@
+// git is version control tool which is used to track source code
+
+// command 
+// git --version version check
+// git init  ==> intialized empty git repo for any folder
+// git status ==>most frequent command to check status of git
+
+// git add .
+// git commit -m 'commit-message'
+
+// whenever you made changes repeat line 8 and 9 to track locally
+
+//  to share your code we have to use one of the service provider(github,gitlab,bitbucket) for hosting
+
+// to synchronize between host provider and local
+// git remote add orgin "remote address";
+
+// along with line 8 and 9 we will add following commnd
+// git push orgin <branch_name> master is default branch
+
+// git add .
+// git commit -m 'commit message'
+// git push orgin master
